c7af84ab2d1435fc0360cde0db8ff47d025268e7
EvaMsanchez <eva_crm20@hotmail.com>
2024-10-15 14:54:10 +0200

    Variantes en el componente registrado con @Configuration y @Bean

--- src/main/java/com/eva/springboot/di/app/springboot_di/AppConfig.java
+++ src/main/java/com/eva/springboot/di/app/springboot_di/AppConfig.java
@@ -1,8 +1,10 @@
 package com.eva.springboot.di.app.springboot_di;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.io.Resource;
 
 import com.eva.springboot.di.app.springboot_di.repositories.ProductRepository;
 import com.eva.springboot.di.app.springboot_di.repositories.ProductRepositoryJson;
@@ -11,11 +13,14 @@ import com.eva.springboot.di.app.springboot_di.repositories.ProductRepositoryJso
 @PropertySource("classpath:config.properties")
 public class AppConfig 
 {
+    @Value("classpath:json/product.json")
+    private Resource resource;
+
     // Bean: para crear registrar un componente spring pero no desde la propia clase, sino desde otra la configuramos
     @Bean("productJson")
     ProductRepository productRepositoryJson()
     {
         // Crean una instancia de una clase creada por nosotros o puede ser una librería, una API
-        return new ProductRepositoryJson();
+        return new ProductRepositoryJson(resource);
     }
 }
--- src/main/java/com/eva/springboot/di/app/springboot_di/repositories/ProductRepositoryJson.java
+++ src/main/java/com/eva/springboot/di/app/springboot_di/repositories/ProductRepositoryJson.java
@@ -8,10 +8,10 @@ import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 
 import com.eva.springboot.di.app.springboot_di.models.Product;
-import com.fasterxml.jackson.core.exc.StreamReadException;
-import com.fasterxml.jackson.databind.DatabindException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+
+// No es un componente, registramos el componente en otra clase de configuración
 public class ProductRepositoryJson implements ProductRepository
 {
     private List<Product> list;
@@ -21,15 +21,21 @@ public class ProductRepositoryJson implements ProductRepository
     {
         // Leer un archivo, primero seleccionamos donde se encuentra
         Resource resource = new ClassPathResource("json/product.json");
+        readValueJson(resource);
+    }
+
+    public ProductRepositoryJson(Resource resource)
+    {
+        readValueJson(resource);
+    }
+
+    private void readValueJson(Resource resource)
+    {
         // Convertir un archivo a un objeto de java
         ObjectMapper objectMapper = new ObjectMapper();
         try {
             // Lee el archivo y luego lo puebla en el objeto Product
-            list = Arrays.asList(objectMapper.readValue(resource.getFile(), Product[].class));
-        } catch (StreamReadException e) {
-            e.printStackTrace();
-        } catch (DatabindException e) {
-            e.printStackTrace();
+            list = Arrays.asList(objectMapper.readValue(resource.getFile(), Product[].class)); // getFile: es igual a getImputStream
         } catch (IOException e) { 
             e.printStackTrace();
         }
